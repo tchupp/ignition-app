8adbd61473339a1e523ccbac2ada652644e20598
theochupp <tclchiam@gmail.com>
2018-12-20 23:55:02 -0500

    Empty selections and exclusions are now stripped out when finding options

--- packages/wasm/crate/src/closet.rs
+++ packages/wasm/crate/src/closet.rs
@@ -31,10 +31,8 @@ pub fn find_outfits(closet: &JsValue, selections: &JsValue, exclusions: &JsValue
         .map(|bytes| bincode::deserialize(&bytes[..]).unwrap())
         .unwrap();
 
-    let selections: Vec<String> = selections.into_serde().unwrap();
-    let exclusions: Vec<String> = exclusions.into_serde().unwrap();
-    let selections: Vec<Item> = selections.into_iter().map(|item| Item::new(item.trim())).collect();
-    let exclusions: Vec<Item> = exclusions.into_iter().map(|item| Item::new(item.trim())).collect();
+    let selections: Vec<Item> = to_items(selections);
+    let exclusions: Vec<Item> = to_items(exclusions);
 
     let outfits = closet.outfits_with(&selections[..], &exclusions[..]);
 
@@ -48,10 +46,8 @@ pub fn find_options(closet: &JsValue, selections: &JsValue, exclusions: &JsValue
         .map(|bytes| bincode::deserialize(&bytes[..]).unwrap())
         .unwrap();
 
-    let selections: Vec<String> = selections.into_serde().unwrap();
-    let exclusions: Vec<String> = exclusions.into_serde().unwrap();
-    let selections: Vec<Item> = selections.into_iter().map(|item| Item::new(item.trim())).collect();
-    let exclusions: Vec<Item> = exclusions.into_iter().map(|item| Item::new(item.trim())).collect();
+    let selections: Vec<Item> = to_items(selections);
+    let exclusions: Vec<Item> = to_items(exclusions);
 
     closet.options(&selections[..], &exclusions[..])
         .map(|options| JsValue::from_serde(&options).unwrap())
@@ -59,4 +55,14 @@ pub fn find_options(closet: &JsValue, selections: &JsValue, exclusions: &JsValue
         .map_err(|err| IgnitionError::from(err))
         .map_err(|err| JsValue::from_serde(&err).unwrap())
         .unwrap_or_else(|err| js_sys::Promise::reject(&err))
+}
+
+fn to_items(items: &JsValue) -> Vec<Item> {
+    let items: Vec<String> = items.into_serde().unwrap();
+
+    items.into_iter()
+        .map(|item| String::from(item.trim()))
+        .filter(|item| !item.is_empty())
+        .map(|item| Item::new(item))
+        .collect()
 }
