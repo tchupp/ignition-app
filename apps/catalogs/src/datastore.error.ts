6aa1ea449643028ff702d463d9ccf7c55ef94d39
theochupp <tclchiam@gmail.com>
2018-12-31 09:21:12 -0500

    Changed DatastoreError type to represent the actual object from Google

--- apps/catalogs/src/datastore.error.ts
+++ apps/catalogs/src/datastore.error.ts
@@ -1,18 +1,20 @@
-export type DatastoreErrorStatus =
-    "ABORTED"
-    | "ALREADY_EXISTS"
-    | "DEADLINE_EXCEEDED"
-    | "FAILED_PRECONDITION"
-    | "INTERNAL"
-    | "INVALID_ARGUMENT"
-    | "NOT_FOUND"
-    | "PERMISSION_DENIED"
-    | "RESOURCE_EXHAUSTED"
-    | "UNAUTHENTICATED"
-    | "UNAVAILABLE";
+import {status} from "grpc";
+
+export enum DatastoreErrorCode {
+    ABORTED = status.ABORTED,
+    ALREADY_EXISTS = status.ALREADY_EXISTS,
+    DEADLINE_EXCEEDED = status.DEADLINE_EXCEEDED,
+    FAILED_PRECONDITION = status.FAILED_PRECONDITION,
+    INTERNAL = status.INTERNAL,
+    INVALID_ARGUMENT = status.INVALID_ARGUMENT,
+    NOT_FOUND = status.NOT_FOUND,
+    PERMISSION_DENIED = status.PERMISSION_DENIED,
+    RESOURCE_EXHAUSTED = status.RESOURCE_EXHAUSTED,
+    UNAUTHENTICATED = status.UNAUTHENTICATED,
+    UNAVAILABLE = status.UNAVAILABLE,
+}
 
 export type DatastoreError = {
-    readonly code: number;
-    readonly message: string;
-    readonly status: DatastoreErrorStatus;
+    readonly code: DatastoreErrorCode;
+    readonly details: string;
 }
